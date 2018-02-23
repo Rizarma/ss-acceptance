569729c5df6f3ba75a0242551451f3bf54fc2c47
Rizki Arya Malik <rizarma@gmail.com>
2018-02-24 02:12:23 +0700

    Create email

--- src/specs/tempMail/createEmail.js
+++ src/specs/tempMail/createEmail.js
@@ -3,4 +3,15 @@ describe('create new email', () => {
         browser.url(process.env.TEMP_MAIL)
         browser.getTitle().should.be.equal('Change e-mail address - Temp Mail')
     })
+
+    it('should be able to type new address', () => {
+        const timestamp = Math.floor(Date.now() / 1000)
+        const mail_address = 'ss-' + timestamp
+
+        browser.setValue('/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/div[1]/div[1]/input[1]', mail_address)
+    })
+
+    it('should be able to save the address', () => {
+        browser.click('//*[@id="postbut"]')
+    })
 })
